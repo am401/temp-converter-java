ea9be64c008da2be6362e5b73b05484ef17833cc
am401 <andras.marton@wpengine.com>
2021-05-23 20:22:01 -0500

    v0.1b improvements in handling user input errors via try / catch statements

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 This is a simple temperature converted (Fahrenheit to Celsius and vice versa) written in Java. The first version of it is just to get more familiar with the syntax.
 
 # CHANGELOG
+## [05-23-2021] - Version 0.1b
+### Added
+- Additional `else if` and `else` statement into the logic where user input is required to calculate F to C and C to F
+- `try` and `catch` statements where user input was asked to ensure we are accurately handling `InputMismatchException` cases
+- Comments to help improve following the code 
 ## [05-22-2021] - Version 0.1a
 ### Added
 - Initial commit and initial class setup
--- src/tempCalc.java
+++ src/tempCalc.java
@@ -1,29 +1,54 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class tempCalc {
     public static void main(String args[]) {
+        int userChoice = 0;
         String choiceOne = "1. Convert to Celsius";
         String choiceTwo = "2. Convert to Fahrenheit";
 
+        // Prompt user for a choice whether they're converting C to F or vice versa
         Scanner choiceMenu = new Scanner(System.in);
         System.out.println("Please select an option:");
         System.out.println(choiceOne);
         System.out.println(choiceTwo);
 
-        int userChoice = choiceMenu.nextInt();
+        // Try statement to capture incorrect user input such as providing a char as
+        // opposed to an integer
+        try {
+            userChoice = choiceMenu.nextInt();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Error with data input: " + e);
+            System.exit(1); // Exit the code if incorrect input was provided
+        }
 
         if (userChoice == 1) {
             Scanner tempFahrenheit = new Scanner(System.in);
             System.out.println("Please provide the Fahrenheit temperature to convert: ");
-            double fahrenheitToConvert = tempFahrenheit.nextDouble();
-            double convertedFahrenheit = (fahrenheitToConvert - 32) * 0.5556;
-            System.out.println("Thank you! Your converted temperature is " + convertedFahrenheit + "°C");
+            try {
+                double fahrenheitToConvert = tempFahrenheit.nextDouble();
+                double convertedFahrenheit = (fahrenheitToConvert - 32) * 0.5556;
+                System.out.println("Thank you! Your converted temperature is " + convertedFahrenheit + "°C");
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Oops, your input cannot be converted.");
+                System.out.println(e); // Print out Java captured error
+            }
+        } else if (userChoice == 2) {
+                Scanner tempCelsius = new Scanner(System.in);
+                System.out.println("Please provide the Celsius temperature to convert: ");
+            try {
+                double celsiusToConvert = tempCelsius.nextDouble();
+                double convertedCelsius = (celsiusToConvert * 1.8) + 32;
+                System.out.println("Thank you! Your converted temperature is " + convertedCelsius + "°F");
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Oops, your input cannot be converted.");
+                System.out.println(e); // Print out Java captured error
+            }
         } else {
-            Scanner tempCelsius = new Scanner(System.in);
-            System.out.println("Please provide the Celsius temperature to convert: ");
-            double celsiusToConvert = tempCelsius.nextDouble();
-            double convertedCelsius = (celsiusToConvert * 1.8) + 32;
-            System.out.println("Thank you! Your converted temperature is " + convertedCelsius + "°F");
+            System.out.println("An incorrect selection was made.");
         }
     }
 }
